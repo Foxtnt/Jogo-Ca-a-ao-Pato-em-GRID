298b192ee9c259629eec3ba212be62245f77412d
Foxtnt <bruno_correa_da_silva@yahoo.com.br>
2026-04-12 17:11:24 -0300

    :sparkles: Segundo commit - Inclusão das variáveis iniciais e comandos para ordenar o grid

--- pato.py
+++ pato.py
@@ -13,3 +13,24 @@ print(Fore.GREEN + ' 1- O jogadore tem 5 disparos;\n 2- O tempo Máximo é de 1 
 nome = input(Fore.BLUE + "Informe o Nome do Jogador: ")
 
 hora_inicial = time.time()
+
+# ----------- Variáveis ----------------
+
+tamanho = 5
+disparos = 5
+
+tempo_limite = 60
+
+# essa linha completa linhas em colunas com a quantidade de 5x5 de ondas
+
+grid = [["🌊"] * tamanho for _ in range (tamanho)]
+
+# sorteio da posição do pato
+
+pato_linha = random.randint(0, tamanho -1)
+pato_coluna = random.randint(0, tamanho -1)
+
+tentativas = []
+
+
+
